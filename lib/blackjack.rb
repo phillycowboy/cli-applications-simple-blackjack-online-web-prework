df5d3d7fc5e0c6b0b158ef88428e28bb7b387393
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-04 19:35:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,11 +48,11 @@ def hit?(number)
   prompt_user
   get_user_input
   # binding.pry
-  # if prompt_user=="h"
-  #   deal_card
+  if user_input=="h"
+    card_total+=deal_card
     # binding.pry
-    # if prompt_user=="s"
-    #   !deal_card
+  elsif user_input=="s"
+    card_total
     # binding.pry
 
 
