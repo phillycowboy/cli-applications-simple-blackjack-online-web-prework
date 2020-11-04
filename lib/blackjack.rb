6cb1d6a7302a03eafcab2151c1719322a0cdd50e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-04 18:42:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,7 +28,7 @@ end
 
 def end_game(number)
   # code #end_game here
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{display_card_total}. Thanks for playing!"
 end
 
 def initial_round
